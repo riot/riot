8ed6550d0a46d5e6154a4328e44a96e3f5a6d1e1
Steve Clay <steve@mrclay.org>
2013-11-01 16:37:30 -0400

    Adds missing semicolons and consistent conditional block braces

--- riot.js
+++ riot.js
@@ -18,12 +18,12 @@
       return (FN[template] = FN[template] || Function("_", "return '" +
          $.trim(template).replace(/\n/g, "").replace(/\{([^\}]+)\}/g, "'+_.$1+'") + "'")
       )(data);
-   }
+   };
 
    // A convenience render method to return a jQuery element
    $.el = function(template, data) {
       return $($.render(template, data));
-   }
+   };
 
    // A classic pattern for separating concerns
    $.observable = function(obj) {
@@ -34,9 +34,11 @@
 
             if (i < 2) {
                jq[name](names, function(e) {
-                  var args = [].slice.call(arguments, 1)
-                  if (names.split(" ")[1]) args.unshift(e.type)
-                  fn.apply(obj, args)
+                  var args = [].slice.call(arguments, 1);
+                  if (names.split(" ")[1]) {
+                     args.unshift(e.type);
+                  }
+                  fn.apply(obj, args);
                })
 
             } else if (i == 2) {
@@ -47,11 +49,11 @@
             }
 
             return obj;
-         }
-      })
+         };
+      });
 
       return obj;
-   }
+   };
 
    // jQueried window object
    win = $(win);
@@ -61,13 +63,16 @@
 
    win.on("load", function(e) {
       setTimeout(function() {
-         if (!page_popped) win.trigger("popstate")
+         if (!page_popped) {
+            win.trigger("popstate");
+         }
       }, 1);
 
    }).on("popstate", function(e) {
-      if (!page_popped) page_popped = true;
-
-   })
+      if (!page_popped) {
+         page_popped = true;
+      }
+   });
 
    // Change the browser URL or listen to changes on the URL
    $.route = function(to) {
@@ -75,16 +80,18 @@
       // listen
       if ($.isFunction(to)) {
          win.on("popstate", function(e, hash) {
-            to(hash || location.hash)
-         })
+            to(hash || location.hash);
+         });
 
       // fire
       } else if (to != location.hash) {
-         if (history.pushState) history.pushState("", "", to)
+         if (history.pushState) {
+            history.pushState("", "", to);
+         }
          win.trigger("popstate", [to]);
       }
 
-   }
+   };
 
 
-})(jQuery, window)
+})(jQuery, window);
