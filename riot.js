5dfe12981993874828232d2e744910d8b3ecddfe
Steve Clay <steve@mrclay.org>
2013-11-01 16:28:14 -0400

    Fixes #9: Limits placeholders to valid (ASCII) JS identifiers

--- riot.js
+++ riot.js
@@ -16,7 +16,7 @@
    // Render a template with data
    $.render = function(template, data) {
       return (FN[template] = FN[template] || Function("_", "return '" +
-         $.trim(template).replace(/\n/g, "").replace(/\{([^\{\}]+)\}/g, "'+_.$1+'") + "'")
+         $.trim(template).replace(/\n/g, "").replace(/\{([a-zA-Z_$][0-9a-zA-Z_$]*)\}/g, "'+_.$1+'") + "'")
       )(data);
    }
 
