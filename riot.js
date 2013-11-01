c1b435bd2d1a4bfb4ff881fac71f35f4e97eae79
Steve Clay <steve@mrclay.org>
2013-11-01 18:20:39 -0400

    Made HTML escaping the default, allows template functions with multiple letters

--- riot.js
+++ riot.js
@@ -9,36 +9,33 @@
 */
 (function($, win) {
 
-   // Precompiled templates (JavaScript functions)
-   var tplFuncs = {};
+   // (JavaScript functions)
+   var compiledTemplates = {};
 
-	// functions injected into the compiled template.
-	var tplEscapers = {
-		// minifiers will not alter object keys
-		r: function (string) { // raw
-			return string;
-		},
-		e: function (string) { // HTML escaped
-			return string.replace(/[&<>"']/g, function (m) { return '&' + m.charCodeAt() + ';'; });
-		}
-	};
-	
-	function compileTpl(template) {
-		var strContent = $.trim(template)
-			.replace(/[\n\\']/g, function (m) { return {"\n":"\\n", "\\":"\\\\", "'":"\\'"}[m]; })
-			.replace(/\{([er]:)?([a-zA-Z_$][0-9a-zA-Z_$]*)\}/g, function (m, f, id) {
-				if (!f) {
-					// default to raw
-					f = "r:";
-				}
-				return "'+(_." + id + "!==undefined?h." + f[0] + "(_." + id + "):'')+'";
-			});
-		return Function('_', 'h', "return '" + strContent + "'");
-	}
+   // functions injected into the compiled template.
+   var templateFunctions = {
+      // minifiers will not alter object keys
+      raw: function (string) { // raw
+         return string;
+      },
+      html: function (string) { // HTML escaped
+         return string.replace(/[&<>"']/g, function (m) { return '&' + m.charCodeAt() + ';'; });
+      }
+   };
+   
+   function compile(template) {
+      var strContent = $.trim(template)
+         .replace(/[\n\\']/g, function (m) { return {"\n":"\\n", "\\":"\\\\", "'":"\\'"}[m]; })
+         .replace(/\{(\w+:)?([a-zA-Z_$][0-9a-zA-Z_$]*)\}/g, function (m, f, id) {
+            f = f || "html:";
+            return "'+(_." + id + "!==undefined?h." + f.substr(0, f.length - 1) + "(_." + id + "):'')+'";
+         });
+      return Function('_', 'h', "return '" + strContent + "'");
+   }
 
    // Render a template with data
    $.render = function(template, data) {
-      return (tplFuncs[template] = tplFuncs[template] || compileTpl(template))(data, tplEscapers);
+      return (compiledTemplates[template] = compiledTemplates[template] || compile(template))(data, templateFunctions);
    }
 
    // A convenience render method to return a jQuery element
