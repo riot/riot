4bc2a17ec353ab7eb6cb1b6b015fe8a0c8234bc4
Tero Piirainen <tipiirai@gmail.com>
2013-11-07 22:29:38 +0200

    Render tokens can be ASCII chars only. Fixes #16

--- test/render.js
+++ test/render.js
@@ -1,10 +1,14 @@
 
 describe("$.render", function() {
 
-   it("Simple ", function() {
+   it("Single token", function() {
       assert($.render("x") == "x");
       assert($.render("x", {}) == "x");
       assert($.render("{x}", { x: "x" }) == "x");
+   })
+
+   it("Multiple tokens", function() {
+      assert($.render("{x}{y}", { x: "x", y: "y" }) == "xy");
    })
 
    it("Single quotes", function() {
@@ -16,5 +20,12 @@ describe("$.render", function() {
       assert($.render("{x}", { x: undefined }) == "");
       assert($.render("{x}", { x: null }) == "");
    })
+
+   it("Nearby brackets", function() {
+      assert($.render("{{x}", { x: 'x' }) == "{x");
+      assert($.render("{x}}", { x: 'x' }) == "x}");
+      assert($.render("{{x}}", { x: 'x' }) == "{x}");
+   })
+
 
 })
