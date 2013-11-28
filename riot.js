3d0e694aaa08626e6a5de1de3b1a174e8e8696cb
Marcelo Eden <edendroid@gmail.com>
2013-11-28 02:17:16 -0800

    satisfy jshint; minify; add test command to makefile;

--- riot.js
+++ riot.js
@@ -67,13 +67,16 @@
   // Render a template with data
   $.render = function(template, data) {
     if(!template) return '';
-    return (FN[template] = FN[template] || new Function("_",
+
+    FN[template] = FN[template] || new Function("_",
       "return '" + template
         .replace(/\n/g, "\\n")
         .replace(/\r/g, "\\r")
         .replace(/'/g, "\\'")
         .replace(/\{\s*(\w+)\s*\}/g, "'+(_.$1?(_.$1+'').replace(/&/g,'&amp;').replace(/\"/g,'&quot;').replace(/</g,'&lt;').replace(/>/g,'&gt;'):(_.$1===0?0:''))+'") + "'"
-    ))(data);
+    );
+
+    return FN[template](data);
   };
 
   // Early return for node
