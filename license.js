1830413fad5c151bb3c62c3f7644d79e7dd33c33
Tero Piirainen <tipiirai@gmail.com>
2013-12-19 20:36:40 +0200

    Back to "1kb framework" (now 1.49kb) by refactoring the build scripts. The router is probably going to change a bit and the file size will eventually be closer to 2kb which is still tolerable.

--- license.js
+++ license.js
@@ -1,8 +1,4 @@
 /*
-	Riot.js 0.9.6 | moot.it/riotjs | @license MIT
+	Riot.js 0.9.7 | moot.it/riotjs | @license MIT
 	(c) 2013 Tero Piirainen, Moot Inc and other contributors.
 */
-(function($) { "use strict";
-  if ($.riot) return;
-  $.riot = "0.9.6";
-})(typeof exports !== "undefined" ? exports : (window.$ = window.$ || {}));
