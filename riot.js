f4b16cfee416ad3b279784ab2b7b7e8b0ce1bfe2
Andrei Lisnic <andrei.lisnic@gmail.com>
2014-01-10 12:13:13 +0200

    Recompile everything

--- riot.js
+++ riot.js
@@ -2,13 +2,12 @@
 	Riot.js 0.9.7 | moot.it/riotjs | @license MIT
 	(c) 2013 Tero Piirainen, Moot Inc and other contributors.
 */
-(function($) { "use strict";
+(function(Riot) { "use strict";
 
-$.observable = function(el) {
+Riot.observable = function(el) {
   var callbacks = {}, slice = [].slice;
 
   el.on = function(events, fn) {
-
     if (typeof fn === "function") {
       events = events.split(/\s+/);
 
@@ -35,40 +34,33 @@ $.observable = function(el) {
   el.one = function(type, fn) {
     if (fn) fn.one = true;
     return el.on(type, fn);
-
   };
 
   el.trigger = function(type) {
-
     var args = slice.call(arguments, 1),
-      fns = callbacks[type] || [];
+        fns  = callbacks[type] || [];
 
     for (var i = 0, fn; i < fns.length; ++i) {
       fn = fns[i];
-
       if (fn.one && fn.done) continue;
 
       // add event argument when multiple listeners
       fn.apply(el, fn.typed ? [type].concat(args) : args);
-
       fn.done = true;
     }
-
     return el;
   };
-
   return el;
-
 };
 
 // Precompiled templates (JavaScript functions)
-var FN = {};
+Riot.templates = {};
 
 // Render a template with data
-$.render = function(template, data) {
+Riot.render = function(template, data) {
   if(!template) return '';
 
-  FN[template] = FN[template] || new Function("_",
+  Riot.templates[template] = Riot.templates[template] || new Function("_",
     "return '" + template
       .replace(/\n/g, "\\n")
       .replace(/\r/g, "\\r")
@@ -76,7 +68,7 @@ $.render = function(template, data) {
       .replace(/\{\s*(\w+)\s*\}/g, "'+(_.$1?(_.$1+'').replace(/&/g,'&amp;').replace(/\"/g,'&quot;').replace(/</g,'&lt;').replace(/>/g,'&gt;'):(_.$1===0?0:''))+'") + "'"
   );
 
-  return FN[template](data);
+  return Riot.templates[template](data);
 };
 
 
@@ -85,9 +77,9 @@ if (typeof top != "object") return;
 
 // cross browser popstate
 var currentHash,
-  pops = $.observable({}),
-  listen = window.addEventListener,
-  doc = document;
+  pops    = $.observable({}),
+  listen  = window.addEventListener,
+  doc     = document;
 
 function pop(hash) {
   hash = hash.type ? location.hash : hash;
@@ -98,7 +90,6 @@ function pop(hash) {
 if (listen) {
   listen("popstate", pop, false);
   doc.addEventListener("DOMContentLoaded", pop, false);
-
 } else {
   doc.attachEvent("onreadystatechange", function() {
     if (doc.readyState === "complete") pop("");
@@ -106,12 +97,11 @@ if (listen) {
 }
 
 // Change the browser URL or listen to changes on the URL
-$.route = function(to) {
+Riot.route = function(to) {
   // listen
   if (typeof to === "function") return pops.on("pop", to);
 
   // fire
   if (history.pushState) history.pushState(0, 0, to);
   pop(to);
-
-};})(typeof top == "object" ? window.$ || (window.$ = {}) : exports);
+};})(typeof top == "object" ? window.Riot || (window.Riot = {}) : exports);
