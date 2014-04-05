cd887b498eee905cacf7f49bb080da0ff65a3665
lloydzhou <lloydzhou@qq.com>
2014-04-05 23:35:59 +0800

    update todomvc demo, using 2 model "TodoItem" and "TodoList". it will make the code easy to understand !

--- todomvc/app.js
+++ todomvc/app.js
@@ -1,63 +1,88 @@
-
 /* The presenter */
 
 (function() { 'use strict';
   /*
     A Model instance exposed to global space so you can
-    use the Todo APi from the console. For example:
+    use the TodoList APi from the console. For example:
 
-    todo.add("My task");
+    todolist.add("My task");
   */
-  window.todo = new Todo();
+  var todolist = window.todolist = new TodoList();
 
   // HTML for a single todo item
   var template = $("[type='html/todo']").html(),
     root = $("#todo-list"),
     nav = $("#filters a");
 
+  /* 
+    bind callback for event "additem" on todolist
+    in this callback add the browser events to the current view (just render by the todoitem)
+  */
+  todolist.on('additem', function(item){
+    var el, input, tbtn, label, dbtn, blur = function (){el.removeClass("editing")};
 
+    /* Listen to model events */
+    /* bind "add", "remove", "toggle", "update" event to TodoItem. */
+    item.on("add", function (){
+      el = $($.render(template, item)).addClass(item.done ? "completed" : "").appendTo(root);
+      input = $(".edit", el); tbtn = $(".toggle", el); label = tbtn.next(); dbtn = label.next();    
+      // bind "click" event to checkbox
+      tbtn.click(function(){ item.toggle() })
+      // bind the "dblclick" event to edit the current item.
+      label.dblclick(function (){
+        el.addClass("editing");
+        input.focus()[0].select();
+      })
+      // bind "click" event to remove button to remove the current item.
+      dbtn.click(function(){ item.trigger('remove') });
+      // bind "blur" and "keydown" event to edit the current item.
+      input.blur(blur).keydown(function(e) {
+        var val = $.trim(this.value);
+        if (e.which == 13 && val) {
+          item.update(val);
+        }
+        if (e.which == 27) blur()
+      })
+    }).on("remove", function() {
+      el.remove()
+      delete todolist[item.id]
+    }).on("toggle", function() { 
+      el.toggleClass("completed", !!item.done);
+      tbtn.prop("checked", !!item.done);
+    }).on("update", function() {
+      el.removeClass("editing");
+      $("label, .edit", el).text(item.name).val(item.name);
+    }).on("add remove toggle", function(){
+      var active = todolist.items("active").length, done = todolist.items("completed").length;
+      $("#todo-count").html("<strong>" +active+ "</strong> item" +(active == 1 ? "" : "s")+ " left")
+      $("#clear-completed").toggle(done > 0).text("Clear completed (" + done + ")")
+      $("#footer").toggle(active + done > 0)
+    }).on("add remove update toggle", function(){
+      todolist.save()
+    });
+  })
 
   /* Listen to user events */
 
   $("#new-todo").keyup(function(e) {
     var val = $.trim(this.value);
     if (e.which == 13 && val) {
-      todo.add(val);
+      todolist.additem({name: val, done:false}).trigger('add')
       this.value = "";
     }
   })
 
   $("#toggle-all").click(function() {
     $("li", root).each(function() {
-      todo.toggle(this.id);
+      todolist[this.id].toggle();
     })
   })
 
   $("#clear-completed").click(function() {
-    todo.remove("completed");
-  })
-
-
-
-  /* Listen to model events */
-
-  todo.on("add", add).on("remove", function(items) {
-    $.each(items, function() {
-      $("#" + this.id).remove()
+    $.each(todolist.items("completed"), function(i,n){
+      n.trigger('remove')
     })
-
-  }).on("toggle", function(item) {
-    toggle($("#" + item.id), !!item.done)
-
-  }).on("edit", function(item) {
-    var el = $("#" + item.id);
-    el.removeClass("editing");
-    $("label, .edit", el).text(item.name).val(item.name);
-
-  // counts
-  }).on("add remove toggle", counts)
-
-
+  })
 
   /* Routing */
 
@@ -66,73 +91,9 @@
   })
 
   $.route(function(hash) {
-
     // clear list and add new ones
-    root.empty() && $.each(todo.items(hash.slice(2)), add)
-
+    root.empty() && todolist.trigger('init').show(hash.slice(2))
     // selected class
     nav.removeClass("selected").filter("[href='" + hash + "']").addClass("selected");
-
-    // update counts
-    counts()
   })
-
-
-
-  /* Private functions */
-
-  function toggle(el, flag) {
-    el.toggleClass("completed", flag);
-    $(":checkbox", el).prop("checked", flag);
-  }
-
-  function add(item) {
-    if (this.id) item = this;
-
-    var el = $($.render(template, item)).appendTo(root),
-      input = $(".edit", el);
-
-
-    $(".toggle", el).click(function() {
-      todo.toggle(item.id);
-    })
-
-    function blur() {
-      el.removeClass("editing")
-    }
-
-    toggle(el, !!item.done);
-
-    // edit
-    input.blur(blur).keydown(function(e) {
-      var val = $.trim(this.value);
-      if (e.which == 13 && val) {
-        item.name = val;
-        todo.edit(item);
-      }
-
-      if (e.which == 27) blur()
-    })
-
-    $("label", el).dblclick(function() {
-      el.addClass("editing");
-      input.focus()[0].select();
-    })
-
-    // remove
-    $(".destroy", el).click(function() {
-      todo.remove(item.id);
-    })
-
-  }
-
-  function counts() {
-    var active = todo.items("active").length,
-       done = todo.items("completed").length;
-
-    $("#todo-count").html("<strong>" +active+ "</strong> item" +(active == 1 ? "" : "s")+ " left")
-    $("#clear-completed").toggle(done > 0).text("Clear completed (" + done + ")")
-    $("#footer").toggle(active + done > 0)
-  }
-
 })()
