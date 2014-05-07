b65317cae6f50bde42eafa70968c652201bc5a27
Tero Piirainen <tipiirai@gmail.com>
2014-05-07 12:54:19 +0300

    V1.0.1

--- license.js
+++ license.js
@@ -1,1 +1,1 @@
-/* Riot 1.0.0, @license MIT, (c) 2014 Muut Inc + contributors */
+/* Riot 1.0.1, @license MIT, (c) 2014 Muut Inc + contributors */
--- riot.js
+++ riot.js
@@ -1,4 +1,4 @@
-/* Riot 1.0.0, @license MIT, (c) 2014 Muut Inc + contributors */
+/* Riot 1.0.1, @license MIT, (c) 2014 Muut Inc + contributors */
 (function(riot) { "use strict";
 
 riot.observable = function(el) {
