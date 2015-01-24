10208e663c5054d2ad83c651919b552c91b5a461
Aurimas <au@rim.as>
2015-01-25 00:46:03 +0700

    Support require('riot') in Browserify

--- compiler/index.js
+++ compiler/index.js
@@ -6,7 +6,7 @@
 //
 // Use in Node:
 //
-//   var riot = require('riot')
+//   var riot = require('riot/compiler')
 //   riot.make({ from: 'foo', to: 'bar', compact: true })
 //   riot.watch({ from: 'foo.js', to: 'bar.js' })
 //
