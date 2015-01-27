42a5aa1313234c1737bba70c93a6933e11d8b122
Tero Piirainen <tipiirai@gmail.com>
2015-01-27 19:17:54 +0200

    Compilation on browser. Initial implementation.

--- compiler/compiler.js
+++ compiler/compiler.js
@@ -1,172 +1,224 @@
 
-var BOOL_ATTR = ('allowfullscreen,async,autofocus,autoplay,checked,compact,controls,declare,default,'+
-  'defaultchecked,defaultmuted,defaultselected,defer,disabled,draggable,enabled,formnovalidate,hidden,'+
-  'indeterminate,inert,ismap,itemscope,loop,multiple,muted,nohref,noresize,noshade,novalidate,nowrap,open,'+
-  'pauseonexit,readonly,required,reversed,scoped,seamless,selected,sortable,spellcheck,translate,truespeed,'+
-  'typemustmatch,visible').split(',')
+
+(function(is_node) {
+
+  var BOOL_ATTR = ('allowfullscreen,async,autofocus,autoplay,checked,compact,controls,declare,default,'+
+    'defaultchecked,defaultmuted,defaultselected,defer,disabled,draggable,enabled,formnovalidate,hidden,'+
+    'indeterminate,inert,ismap,itemscope,loop,multiple,muted,nohref,noresize,noshade,novalidate,nowrap,open,'+
+    'pauseonexit,readonly,required,reversed,scoped,seamless,selected,sortable,spellcheck,translate,truespeed,'+
+    'typemustmatch,visible').split(',')
 
 
-// (tagname) (html) (javascript) endtag
-var CUSTOM_TAG = /^<([\w\-]+)>([^\x00]*[\w\/]>$)([^\x00]*?)^<\/\1>/gim,
-    SCRIPT = /<script(\s+type=['"]?([^>'"]+)['"]?)?>([^\x00]*?)<\/script>/gm,
-    HTML_COMMENT = /<!--.*?-->/g,
-    CLOSED_TAG = /<([\w\-]+)([^\/]*)\/\s*>/g,
-    LINE_COMMENT = /^\s*\/\/.*$/gm,
-    JS_COMMENT = /\/\*[^\x00]*?\*\//gm
+  // (tagname) (html) (javascript) endtag
+  var CUSTOM_TAG = /^<([\w\-]+)>([^\x00]*[\w\/]>$)([^\x00]*?)^<\/\1>/gim,
+      SCRIPT = /<script(\s+type=['"]?([^>'"]+)['"]?)?>([^\x00]*?)<\/script>/gm,
+      HTML_COMMENT = /<!--.*?-->/g,
+      CLOSED_TAG = /<([\w\-]+)([^\/]*)\/\s*>/g,
+      LINE_COMMENT = /^\s*\/\/.*$/gm,
+      JS_COMMENT = /\/\*[^\x00]*?\*\//gm
 
 
-function compileHTML(html, opts, type) {
+  function compileHTML(html, opts, type) {
 
-  // whitespace
-  html = html.replace(/\s+/g, ' ')
+    // whitespace
+    html = html.replace(/\s+/g, ' ')
 
-  // strip comments
-  html = html.trim().replace(HTML_COMMENT, '')
+    // strip comments
+    html = html.trim().replace(HTML_COMMENT, '')
 
-  // foo={ bar } --> foo="{ bar }"
-  html = html.replace(/=(\{[^\}]+\})([\s\>])/g, '="$1"$2')
+    // foo={ bar } --> foo="{ bar }"
+    html = html.replace(/=(\{[^\}]+\})([\s\>])/g, '="$1"$2')
 
-  // IE8 looses boolean attr values: `checked={ expr }` --> `__checked={ expr }`
-  html = html.replace(/([\w\-]+)=["'](\{[^\}]+\})["']/g, function(full, name, expr) {
-    if (BOOL_ATTR.indexOf(name.toLowerCase()) >= 0) name = '__' + name
-    return name + '="' + expr + '"'
-  })
-
-  // run trough parser
-  if (opts.expr) {
-    html = html.replace(/\{\s*([^\}]+)\s*\}/g, function(_, expr) {
-       return '{' + compileJS(expr, opts, type).trim() + '}'
+    // IE8 looses boolean attr values: `checked={ expr }` --> `__checked={ expr }`
+    html = html.replace(/([\w\-]+)=["'](\{[^\}]+\})["']/g, function(full, name, expr) {
+      if (BOOL_ATTR.indexOf(name.toLowerCase()) >= 0) name = '__' + name
+      return name + '="' + expr + '"'
     })
-  }
 
-  // <foo/> -> <foo></foo>
-  html = html.replace(CLOSED_TAG, function(_, tagName, attr) {
-    return '<' + tagName + (attr ? ' ' + attr.trim() : '') + '></' + tagName + '>'
-  })
-
-  // escape single quotes
-  html = html.replace(/'/g, "\\'")
-
-
-  // \{ jotain \} --> \\{ jotain \\}
-  html = html.replace(/\\[{}]/g, '\\$&')
-
-  // compact: no whitespace between tags
-  if (opts.compact) html = html.replace(/> </g, '><')
-
-  return html
-
-}
-
-function coffee(js) {
-  return require('coffee-script').compile(js, { bare: true })
-}
-
-function es6(js) {
-  return require('6to5').transform(js).code
-}
-
-function typescript(js) {
-  return require('typescript-simple')(js)
-}
-
-function jade(html) {
-  return require('jade').render(html, {pretty: true})
-}
-
-function plainjs(js) {
-  return js
-}
-
-function riotjs(js) {
-
-  // strip comments
-  js = js.replace(LINE_COMMENT, '').replace(JS_COMMENT, '')
-
-  // ES6 method signatures
-  var lines = js.split('\n'),
-      es6_ident = ''
-
-  lines.forEach(function(line, i) {
-    var l = line.trim()
-
-    // method start
-    if (l[0] != '}' && l.indexOf('(') > 0 && l.slice(-1) == '{' && l.indexOf('function') == -1) {
-      var m = /(\s+)([\w]+)\s*\(([\w,\s]*)\)\s*\{/.exec(line)
-
-      if (m && !/^(if|while|switch|for)$/.test(m[2])) {
-        lines[i] = m[1] + 'this.' + m[2] + ' = function(' + m[3] + ') {'
-        es6_ident = m[1]
-      }
-
-    }
-
-    // method end
-    if (line.slice(0, es6_ident.length + 1) == es6_ident + '}') {
-      lines[i] += '.bind(this);'
-      es6_ident = ''
-    }
-
-  })
-
-  return lines.join('\n')
-
-}
-
-var PARSERS = {
-  coffeescript: coffee,
-  none: plainjs,
-  cs: coffee,
-  es6: es6,
-  typescript: typescript
-}
-
-var TEMPLATE_PARSERS = {
-  jade: jade
-}
-
-function compileJS(js, opts, type) {
-  var parser = opts.parser || (type ? PARSERS[type] : riotjs)
-  if (!parser) throw new Error('Parser not found "' + type + '"')
-  return parser(js, opts)
-}
-
-function compileTemplate(lang, html) {
-  var parser = TEMPLATE_PARSERS[lang]
-  if (!parser) throw new Error('Template parser not found "' + lang + '"')
-  return parser(html)
-}
-
-function compile(riot_tag, opts) {
-
-  opts = opts || {}
-
-  if (opts.template) {
-    riot_tag = compileTemplate(opts.template, riot_tag)
-  }
-
-  return riot_tag.replace(CUSTOM_TAG, function(_, tagName, html, js) {
-
-    // js wrapped inside <script> tag
-    var type = opts.type
-
-    if (!js.trim()) {
-      html = html.replace(SCRIPT, function(_, fullType, _type, script) {
-        if (_type) type = _type.replace('text/', '')
-        js = script
-        return ''
+    // run trough parser
+    if (opts.expr) {
+      html = html.replace(/\{\s*([^\}]+)\s*\}/g, function(_, expr) {
+         return '{' + compileJS(expr, opts, type).trim() + '}'
       })
     }
 
-    return 'riot.tag(\'' +tagName+ '\', \'' + compileHTML(html, opts, type) + '\', function(opts) {' +
-      compileJS(js, opts, type) +
-    '\n});'
+    // <foo/> -> <foo></foo>
+    html = html.replace(CLOSED_TAG, function(_, tagName, attr) {
+      return '<' + tagName + (attr ? ' ' + attr.trim() : '') + '></' + tagName + '>'
+    })
 
-  })
+    // escape single quotes
+    html = html.replace(/'/g, "\\'")
 
-}
 
-module.exports = {
-  html: compileHTML,
-  compile: compile
-}
+    // \{ jotain \} --> \\{ jotain \\}
+    html = html.replace(/\\[{}]/g, '\\$&')
+
+    // compact: no whitespace between tags
+    if (opts.compact) html = html.replace(/> </g, '><')
+
+    return html
+
+  }
+
+  function coffee(js) {
+    return require('coffee-script').compile(js, { bare: true })
+  }
+
+  function es6(js) {
+    return require('6to5').transform(js).code
+  }
+
+  function plainjs(js) {
+    return js
+  }
+
+  function typescript(js) {
+    return require('typescript-simple')(js)
+  }
+
+  function plainjs(js) {
+    return js
+  }
+
+  function jade(html) {
+    return require('jade').render(html, {pretty: true})
+  }
+
+  function riotjs(js) {
+
+    // strip comments
+    js = js.replace(LINE_COMMENT, '').replace(JS_COMMENT, '')
+
+    // ES6 method signatures
+    var lines = js.split('\n'),
+        es6_ident = ''
+
+    lines.forEach(function(line, i) {
+      var l = line.trim()
+
+      // method start
+      if (l[0] != '}' && l.indexOf('(') > 0 && l.slice(-1) == '{' && l.indexOf('function') == -1) {
+        var m = /(\s+)([\w]+)\s*\(([\w,\s]*)\)\s*\{/.exec(line)
+
+        if (m && !/^(if|while|switch|for)$/.test(m[2])) {
+          lines[i] = m[1] + 'this.' + m[2] + ' = function(' + m[3] + ') {'
+          es6_ident = m[1]
+        }
+
+      }
+
+      // method end
+      if (line.slice(0, es6_ident.length + 1) == es6_ident + '}') {
+        lines[i] += '.bind(this);'
+        es6_ident = ''
+      }
+
+    })
+
+    return lines.join('\n')
+
+  }
+
+  var HTML_PARSERS = {
+    jade: jade
+
+  }, JS_PARSERS = {
+    coffeescript: coffee,
+    none: plainjs,
+    cs: coffee,
+    es6: es6,
+    typescript: typescript
+  }
+
+
+  function compileJS(js, opts, type) {
+    var parser = opts.parser || (type ? JS_PARSERS[type] : riotjs)
+    if (!parser) throw new Error('Parser not found "' + type + '"')
+    return parser(js, opts)
+  }
+
+  function compileTemplate(lang, html) {
+    var parser = HTML_PARSERS[lang]
+    if (!parser) throw new Error('Template parser not found "' + lang + '"')
+    return parser(html)
+  }
+
+  function compile(riot_tag, opts) {
+
+    opts = opts || {}
+
+    if (opts.template) {
+      riot_tag = compileTemplate(opts.template, riot_tag)
+    }
+
+    return riot_tag.replace(CUSTOM_TAG, function(_, tagName, html, js) {
+
+      // js wrapped inside <script> tag
+      var type = opts.type
+
+      if (!js.trim()) {
+        html = html.replace(SCRIPT, function(_, fullType, _type, script) {
+          if (_type) type = _type.replace('text/', '')
+          js = script
+          return ''
+        })
+      }
+
+      return 'riot.tag(\'' +tagName+ '\', \'' + compileHTML(html, opts, type) + '\', function(opts) {' +
+        compileJS(js, opts, type) +
+      '\n});'
+
+    })
+
+  }
+
+
+  // node and io.js
+  if (is_node) {
+    return module.exports = {
+      html: compileHTML,
+      compile: compile
+    }
+  }
+
+  // browsers
+  var doc = document
+
+  function GET(url, fn) {
+    var req = new XMLHttpRequest()
+
+    req.onload = function() {
+      if (req.status >= 200 && req.status < 400) fn(req.responseText)
+    }
+    req.open('GET', url, true)
+    req.send('')
+  }
+
+  function unindent(src) {
+    var ident = /[ \t]+/.exec(src)[0],
+        re = new RegExp('^' + ident, 'gm')
+    return src.replace(re, '')
+  }
+
+  riot.compile = function(fn) {
+    var scripts = doc.querySelectorAll('script[type="riot/tag"]')
+
+    ;[].map.call(scripts, function(script, i) {
+      var url = script.getAttribute('src')
+
+      function addTag(html) {
+        script.parentNode.removeChild(script)
+        var node = doc.createElement('script')
+        node.innerHTML = compile(html)
+        doc.documentElement.appendChild(node)
+        if (i + 1 == scripts.length) fn()
+      }
+
+      return url ? GET(url, addTag) : addTag(unindent(script.innerHTML))
+
+    })
+
+  }
+
+})(!this.top)
