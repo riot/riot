c98ca646d81f588cd6caae004de244cca106dd4d
Tero Piirainen <tipiirai@gmail.com>
2015-01-28 12:44:49 +0200

    In-browser compiler docs

--- compiler/compiler.js
+++ compiler/compiler.js
@@ -196,9 +196,12 @@
   }
 
   function globalEval(js) {
-    var node = doc.createElement('script')
+    var node = doc.createElement('script'),
+        root = doc.documentElement
+
     node.text = compile(js)
-    doc.documentElement.appendChild(node)
+    root.appendChild(node)
+    root.removeChild(node)
   }
 
   function compileScripts(fn) {
@@ -223,10 +226,14 @@
 
   }
 
-  riot.compile = function(arg) {
+  riot.compile = function(arg, skip_eval) {
 
     // string -> compile a new tag
-    if (typeof arg == 'string') return globalEval(unindent(compile(arg)))
+    if (typeof arg == 'string') {
+      var js = unindent(compile(arg))
+      if (!skip_eval) globalEval(js)
+      return js
+    }
 
     // must be a function
     if (typeof arg != 'function') arg = undefined
