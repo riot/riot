4c98fe9b4c19c5b7b270451cfc65a43242e11e69
Tero Piirainen <tipiirai@gmail.com>
2015-02-05 15:02:55 -0500

    riot.mount returns tag instances also when compiled on browser, #282

---
+++ make/dev.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+// For Riot developers
+
+require('shelljs/global')
+
+var chokidar = require('chokidar'),
+    cli = require('../lib/cli')
+
+
+// watch and build riot.js
+chokidar
+  .watch('lib/tag/*.js', { ignoreInitial: true })
+  .on('all', function() {
+    ('var riot = {}; (function(riot, is_browser) {' +
+      cat('lib/observable.js', 'lib/tmpl.js', 'lib/tag/*') +
+    '})(riot, this.top)').to('dist/riot.js')
+  })
+
+
+// watch and build tags.js for testing
+cli.watch({ from: 'test/tag', to: 'dist/tags.js' })
