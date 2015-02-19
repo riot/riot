67664742777d47e636211f2eca4cdfc5b16fb779
Kalman Speier <kalman.speier@gmail.com>
2015-02-19 17:05:55 +0100

    simplified require tag extension

--- test/commonjs.js
+++ test/commonjs.js
@@ -1,12 +1,7 @@
 var test = require('tape')
-
-var riot = require('..').register()
-var timer = require('./tag/timer.tag')
+var riot = require('..')
 
 test('riot require extension test', function(t) {
-  t.plan(4)
-  t.equal(timer.name, 'timer', 'tag name should be ok')
-  t.equal(timer.tmpl, '<p>Seconds Elapsed: { time }</p>', 'tag html should be ok')
-  t.equal(timer.css, '', 'tag css should be empty')
-  t.equal(typeof timer.fn, 'function', 'tag `fn` should be a function')
+  t.plan(1)
+  t.ok(require('./tag/timer.tag'), 'requiring tag file')
 })
