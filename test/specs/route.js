e8fe50a54b5f39527bfd6fd7a9cf6a0fe3ef4f20
Gianluca Guarini <gianluca.guarini@gmail.com>
2015-02-20 10:03:29 +0100

    route tests

---
+++ test/specs/route.js
@@ -0,0 +1,45 @@
+describe('route', function() {
+
+  var counter = 0
+
+  it('it detecs the hash params', function() {
+
+    riot.route(function(first, second, params) {
+      counter++
+      expect(~['mummypowder', '!'].indexOf(first)).to.not.be(false)
+      expect(~['logo-and-key', 'user'].indexOf(second)).to.not.be(false)
+      expect(~[undefined, 'activation?token=xyz'].indexOf(params)).to.not.be(false)
+    })
+
+    riot.route.exec(function(first, second, params) {
+      counter++
+      expect(~['', '!'].indexOf(first)).to.not.be(false)
+      expect(~[undefined, 'user'].indexOf(second)).to.not.be(false)
+      expect(~[undefined, 'activation?token=xyz'].indexOf(params)).to.not.be(false)
+    })
+
+    riot.route('mummypowder/logo-and-key')
+
+    false && riot.route.parser(function(path) {
+      var raw = path.slice(2).split('?'),
+          uri = raw[0].split('/'),
+          qs = raw[1],
+          params = {}
+
+      if (qs) {
+        qs.split('&').forEach(function(v) {
+          var c = v.split('=')
+          params[c[0]] = c[1]
+        })
+      }
+
+      uri.push(params)
+      return uri
+    })
+
+    riot.route('!/user/activation?token=xyz')
+
+    expect(counter).to.be(3)
+
+  })
+})
