784e046f15e691154dc6fecb088e43fe56b99a44
Kalman Speier <kalman.speier@gmail.com>
2015-02-21 09:12:55 +0100

    adding require extension without modifying riot compiler

--- test/commonjs.js
+++ test/commonjs.js
@@ -1,6 +1,6 @@
 var assert = require('assert')
 
+process.env.RIOT = '../dist/riot/riot'
 var riot = require('..')
 
 assert.ok(require('./tag/timer.tag'))
-assert.equal(riot.render('timer', { start: 10 }), '<p>Seconds Elapsed: 10</p>')
