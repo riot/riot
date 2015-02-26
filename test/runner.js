86c68de33ccfa93d8593f3b8beaab83f462f134e
Gianluca Guarini <gianluca.guarini@gmail.com>
2015-02-26 22:40:05 +0100

    better tests coverage using karma phantomjs

--- test/runner.js
+++ test/runner.js
@@ -4,16 +4,8 @@ var isNode = typeof window === 'undefined'
 describe('Riotjs tests', function() {
   if (isNode) {
 
-    global.document = require('jsdom').jsdom()
-    global.window = global.document.defaultView
-    global.location = window.location
-    global.top = window
-
-    require('shelljs/global')
-    global.riot = require('../dist/riot/riot')
     global.compiler = require('../lib/compiler')
     global.expect = require('expect.js')
-
     require('./specs/compiler-cli') // TODO: fix some tests
     //require('./specs/compiler-browser')
     require('./specs/tmpl')
