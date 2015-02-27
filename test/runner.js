6124f56525c99bc35826b00ba9850c04f022da1f
Gianluca Guarini <gianluca.guarini@gmail.com>
2015-02-27 09:40:50 +0100

    fix the tests

--- test/runner.js
+++ test/runner.js
@@ -7,11 +7,6 @@ describe('Riotjs tests', function() {
     global.compiler = require('../lib/compiler')
     global.expect = require('expect.js')
     require('./specs/compiler-cli') // TODO: fix some tests
-    //require('./specs/compiler-browser')
-    require('./specs/tmpl')
-    require('./specs/observable')
-    require('./specs/route')
-    require('./specs/scoped-css')
 
   } else {
     mocha.run()
