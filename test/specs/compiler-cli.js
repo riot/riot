29faf2c0e923578d31e578a5946ce7aee15ab0c1
Gianluca Guarini <gianluca.guarini@gmail.com>
2015-03-06 10:11:31 +0100

    removed useless comment in the compiler tests

--- test/specs/compiler-cli.js
+++ test/specs/compiler-cli.js
@@ -5,33 +5,4 @@ describe('Compiler CLI', function() {
   require('./compiler/riotjs')
   require('./compiler/tag')
 
-/*
-
-  TODO: fix these tests
-  // they depend on the dependecies installed on the local machine
-
-  it('files', function() {
-
-    function test (name, opts) {
-
-      var type = opts.type,
-          dir = 'test/compiler',
-          basename = name + (type ? '.' + type : ''),
-          src = cat(dir + '/' + basename + '.tag'),
-          should = cat(dir + '/js/' + basename + '.js')
-
-      expect(compiler.compile(src, opts).trim()).to.be.equal(should)
-
-    }
-
-    test('complex', {})
-    test('test', { type: 'cs', expr: true })
-    test('test', { type: 'es6' })
-    test('test.jade', { template: 'jade' })
-    test('slide.jade', { template: 'jade' })
-    test('style', {})
-    test('brackets', { brackets: '${ }' })
-
-  })
-*/
 })
