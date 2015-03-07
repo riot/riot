a0b4c88c60fb72f977051c165908dd4b6c332494
Gianluca Guarini <gianluca.guarini@gmail.com>
2015-03-07 15:50:16 +0100

    lighter test

--- test/specs/speed.js
+++ test/specs/speed.js
@@ -7,15 +7,13 @@ describe('Speed', function() {
   })
 
   after(function() {
-    this.timeout(10000)
     tag.unmount()
   })
 
-  it('it must render a list of 10000 items in less than a second', function(done) {
+  it('it must render a list of 2000 items in less than a second', function() {
     var time,
-        renderingTime = 5000 // this must be a looooot faster
+        renderingTime = 1000 // this must be a looooot faster
     // this is going to be slow
-    this.timeout(10000)
     riot.tag('my-tag', '<div> <h1><button onclick="{ clicked }">reverse list</button></h1> <h2 each="{ item, i in opts.items }" id="{ first-item: i === 0 }">{ item.value }</h2> </div>', function(opts) {
       var self = this
       this.items = opts.items
@@ -33,7 +31,7 @@ describe('Speed', function() {
     // mount the tag
     var items = []
 
-    for ( var i = 0; i < 10000; i++ ) {
+    for ( var i = 0; i < 2000; i++ ) {
       items.push( { value: i } )
     }
 
@@ -42,8 +40,7 @@ describe('Speed', function() {
 
     tag.clicked()
     expect(time).to.be.below(renderingTime)
-    expect(~~document.getElementById('first-item').innerHTML).to.be(9999)
-    done()
+    expect(~~document.getElementById('first-item').innerHTML).to.be(1999)
 
   })
 
