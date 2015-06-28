f11ffee5d650816cb7be15388c433fbc513522c0
Gianluca Guarini <gianluca.guarini@gmail.com>
2015-06-28 15:47:40 +0200

    updated: todo example

--- demo/todo.js
+++ demo/todo.js
@@ -1,28 +1,26 @@
 
-riot.tag('todo', '<h3>{ opts.title }</h3> <ul> <li each="{ items.filter(filter) }"> <label class="{ completed: done }"> <input type="checkbox" __checked="{ done }" onclick="{ parent.toggle }"> { title } </label> </li> </ul> <form onsubmit="{ add }"> <input name="input" onkeyup="{ edit }"> <button __disabled="{ !text }">Add #{ items.filter(filter).length + 1 }</button> </form>', function(opts) {
+riot.tag('todo', '<h3>{ opts.title }</h3> <ul> <li each="{ items.filter(filter) }"> <label class="{ completed: done }"> <input type="checkbox" __checked="{ done }" onclick="{ parent.toggle }"> { title } </label> </li> </ul> <form onsubmit="{ add }"> <input name="input" onkeyup="{ edit }"> <button __disabled="{ !text }">Add #{ items.filter(filter).length + 1 }</button> </form> ', function(opts) {
+    this.items = opts.items
 
-  this.items = opts.items
+    this.edit = function(e) {
+      this.text = e.target.value
+    }.bind(this);
 
-  this.edit = function(e) {
-    this.text = e.target.value
-  }.bind(this);
+    this.add = function(e) {
+      if (this.text) {
+        this.items.push({ title: this.text, done: false, hidden: false })
+        this.text = this.input.value = ''
+      }
+    }.bind(this);
 
-  this.add = function(e) {
-    if (this.text) {
-      this.items.push({ title: this.text })
-      this.text = this.input.value = ''
-    }
-  }.bind(this);
+    this.filter = function(item) {
+      return !item.hidden
+    }.bind(this);
 
-  this.filter = function(item) {
-    return !item.hidden
-  }.bind(this);
-
-  this.toggle = function(e) {
-    var item = e.item
-    item.done = !item.done
-    return true
-  }.bind(this);
-
-
+    this.toggle = function(e) {
+      var item = e.item
+      item.done = !item.done
+      return true
+    }.bind(this);
+  
 });
