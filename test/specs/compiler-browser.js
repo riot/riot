54167610d00bd699c664a0870af72c04e7294599
Gianluca Guarini <gianluca.guarini@gmail.com>
2015-08-01 18:42:45 +0200

    removed: duplicate code from the compiler tests

--- test/specs/compiler-browser.js
+++ test/specs/compiler-browser.js
@@ -41,20 +41,6 @@ document.contains = Element.prototype.contains = function contains(node) {
   return false
 }
 
-// small polyfill
-// normalize the document.contains method
-document.contains = Element.prototype.contains = function contains(node) {
-  if (!(0 in arguments)) {
-    throw new TypeError('1 argument is required')
-  }
-  do {
-    if (this === node) {
-      return true
-    }
-  } while (node = node && node.parentNode)
-  return false
-}
-
 describe('Compiler Browser', function() {
 
   var html = [
