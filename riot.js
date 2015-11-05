c7f345a9853e1612f07117932c55e328aec36cf2
Gianluca Guarini <gianluca.guarini@gmail.com>
2015-11-05 23:40:19 +0100

    2.3.0

--- riot.js
+++ riot.js
@@ -1,8 +1,8 @@
-/* Riot v2.3.0-beta, @license MIT, (c) 2015 Muut Inc. + contributors */
+/* Riot v2.3.0, @license MIT, (c) 2015 Muut Inc. + contributors */
 
 ;(function(window, undefined) {
   'use strict';
-var riot = { version: 'v2.3.0-beta', settings: {} },
+var riot = { version: 'v2.3.0', settings: {} },
   // be aware, internal usage
   // ATTENTION: prefix the global dynamic variables with `__`
 
