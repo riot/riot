231ac231092308455f7d6c2fff5c5d50391bb750
Gianluca Guarini <gianluca.guarini@gmail.com>
2015-12-05 22:24:14 +0100

    small update to the yield test

--- test/specs/browser/compiler.js
+++ test/specs/browser/compiler.js
@@ -753,11 +753,10 @@ describe('Compiler Browser', function() {
   })
 
   it('<yield> from/to multi-transclusion', function() {
-
     injectHTML('<yield-multi><yield to="content">content</yield></yield-multi>')
     var tag = riot.mount('yield-multi', {})[0]
     expect(normalizeHTML(tag.root.innerHTML)).to.be('<p>yield the content here</p>')
-
+    tags.push(tag)
   })
 
   it('multiple mount <yield> tag', function() {
