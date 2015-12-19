46781f99da4120173785730a7da79ee4f650efdb
amarcruz <amarcruzbox-git@yahoo.com>
2015-12-19 12:29:35 -0600

    SauceLabs fix 1

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -41,7 +41,7 @@ module.exports = function(config) {
       build: 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')',
       tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
       testName: 'riotjs',
-      startConnect: true,
+      startConnect: false,  // karma-runner/karma-sauce-launcher#73
       recordVideo: false,
       recordScreenshots: false
     },
