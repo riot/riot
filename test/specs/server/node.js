86b7a22a56f80e74920ce436396f596eef0ffd76
Gianluca Guarini <gianluca.guarini@gmail.com>
2016-02-27 17:39:01 +0100

    closes #1628

--- test/specs/server/node.js
+++ test/specs/server/node.js
@@ -81,6 +81,12 @@ describe('Node/io.js', function() {
     expect(els.last().text()).to.be('post 2')
   })
 
+  it('tender tag: loop table', function() {
+    var tbl = riot.render('table-loop-extra-row'),
+      $ = cheerio.load(tbl)
+    expect($('table tr').length).to.be(5)
+  })
+
   it('render tag: simple block (using yield)', function() {
     var blk = riot.render('block')
     var $ = cheerio.load(blk)
