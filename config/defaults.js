489af158302b8b3bf0d7f592e9d7fdc0850c6339
Gianluca Guarini <gianluca.guarini@gmail.com>
2016-05-25 19:59:36 +0200

    updated: fixed output code style

--- config/defaults.js
+++ config/defaults.js
@@ -6,11 +6,11 @@ module.exports = {
   format: 'umd',
   moduleName: 'riot',
   banner: '/* Riot WIP, @license MIT */',
-  indent: false,
   plugins: [
     nodeResolve({ jsnext: true, main: true }),
     commonjs({ include: 'node_modules/**' }),
     babel({
+      compact: false,
       exclude: 'node_modules/riot-tmpl/**'
     })
   ]
