0d52475c327f52bc6229b483eb10952207aff755
Gianluca Guarini <gianluca.guarini@gmail.com>
2016-12-14 08:27:54 +0100

    closes #2156

--- test/specs/browser/show.spec.js
+++ test/specs/browser/show.spec.js
@@ -1,7 +1,8 @@
 
 import {
   injectHTML,
-  $$
+  $$,
+  $
 } from '../../helpers/index'
 
 import '../../tag/show-bug-2125.tag'
@@ -18,5 +19,21 @@ it('the show directive evaluates also the parent properties', function() {
   expect(children[0].style.display).to.be.not.equal('none')
   expect(children[1].style.display).to.be.equal('none')
 
+  tag.unmount()
+})
+
+it('the show directive works as expected', function() {
+  riot.tag('riot-show-tmp', '<p show="{ isVisible }">foo</p>')
+  injectHTML('<riot-show-tmp></riot-show-tmp>')
+  var tag = riot.mount('riot-show-tmp')[0],
+    p = $('p', tag.root)
+
+  expect(p.style.display).to.be.equal('none')
+  tag.isVisible = true
+  tag.update()
+  expect(p.style.display).to.be.not.equal('none')
+
+  // teardown
+  riot.unregister('riot-show-tmp')
   tag.unmount()
 })
