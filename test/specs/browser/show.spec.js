4e918248e02c6aedd1fafbc897a5919a3fb89769
Gianluca Guarini <gianluca.guarini@gmail.com>
2017-01-10 12:03:11 +0100

    closes #2158 make the if and show behavior consistent

--- test/specs/browser/show.spec.js
+++ test/specs/browser/show.spec.js
@@ -1,26 +1,10 @@
 
 import {
   injectHTML,
-  $$,
   $
 } from '../../helpers/index'
 
-import '../../tag/show-bug-2125.tag'
-
 const expect = chai.expect
-
-it('the show directive evaluates also the parent properties', function() {
-  injectHTML('<show-bug-2125></show-bug-2125>')
-  var tag = riot.mount('show-bug-2125')[0],
-    children = $$('show-bug-2125-child', tag.root)
-
-  expect(tag.tags['show-bug-2125-child']).to.have.length(2)
-  expect(children).to.have.length(2)
-  expect(children[0].style.display).to.be.not.equal('none')
-  expect(children[1].style.display).to.be.equal('none')
-
-  tag.unmount()
-})
 
 it('the show directive works as expected', function() {
   riot.tag('riot-show-tmp', '<p show="{ isVisible }">foo</p>')
