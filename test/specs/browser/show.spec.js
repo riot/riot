794fccf3f6e2b5f8a2aeca32a2e02e7b0c44ae78
Gianluca Guarini <gianluca.guarini@gmail.com>
2017-03-26 19:24:51 +0200

    closes #2178

--- test/specs/browser/show.spec.js
+++ test/specs/browser/show.spec.js
@@ -1,6 +1,7 @@
 
 import {
   injectHTML,
+  $,
   $$
 } from '../../helpers/index'
 
@@ -14,18 +15,41 @@ describe('Riot show/hide', function() {
       p = $$('p', tag.root)
 
     expect(p[0].style.display).to.be.equal('none')
+    expect(p[0].hidden).to.be.ok
     expect(p[1].style.display).to.be.not.equal('none')
+    expect(p[1].hidden).to.be.not.ok
     tag.isVisible = true
     tag.update()
     expect(p[0].style.display).to.be.not.equal('none')
+    expect(p[0].hidden).to.be.not.ok
     expect(p[1].style.display).to.be.equal('none')
+    expect(p[1].hidden).to.be.ok
     tag.isVisible = false
     tag.update()
     expect(p[0].style.display).to.be.equal('none')
+    expect(p[0].hidden).to.be.ok
     expect(p[1].style.display).to.be.not.equal('none')
+    expect(p[1].hidden).to.be.not.ok
 
     // teardown
     riot.unregister('riot-show-tmp')
     tag.unmount()
+  })
+
+  it('the show directive gets preserved also in case of style expressions updates', function() {
+    riot.tag('riot-show-tmp', '<p show="{ isVisible }" style="{ \'color:red\' }">foo</p>')
+    injectHTML('<riot-show-tmp></riot-show-tmp>')
+    var tag = riot.mount('riot-show-tmp')[0],
+      p = $('p', tag.root)
+
+    expect(p.style.display).to.be.equal('none')
+    expect(p.hidden).to.be.ok
+    expect(p.style.color).to.be.equal('red')
+    tag.isVisible = true
+    tag.update()
+
+    expect(p.style.display).to.be.not.equal('none')
+    expect(p.hidden).to.be.not.ok
+    expect(p.style.color).to.be.equal('red')
   })
 })
