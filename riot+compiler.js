1040021461b55e1b3646138aa498abf50e52c91c
Gianluca Guarini <gianluca.guarini@gmail.com>
2018-06-16 22:36:23 +0200

    3.10.2

--- riot+compiler.js
+++ riot+compiler.js
@@ -1,4 +1,4 @@
-/* Riot v3.10.1, @license MIT */
+/* Riot v3.10.2, @license MIT */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
@@ -2078,7 +2078,7 @@
     return delete __TAG_IMPL[name]
   }
 
-  var version = 'v3.10.1';
+  var version = 'v3.10.2';
 
   var core = /*#__PURE__*/Object.freeze({
     Tag: Tag,
@@ -2393,7 +2393,7 @@
         // does a tag exist in this position?
         var tag = tags[pos];
         var mustAppend = i >= oldItems.length;
-        var mustCreate = doReorder && isNew || !doReorder && !tag;
+        var mustCreate = doReorder && isNew || !doReorder && !tag || !tags[i];
 
         // new tag
         if (mustCreate) {
@@ -3085,7 +3085,7 @@
 
   /**
    * Compiler for riot custom tags
-   * @version v3.4.0
+   * @version v3.5.1
    */
 
   // istanbul ignore next
@@ -3523,7 +3523,7 @@
 
       html = html.trim().replace(/\s+/g, ' ');
 
-      if (p.length) { html = html.replace(/\u0002/g, function () { return p.shift() }); }
+      if (p.length) { html = html.replace(/\u0002/g, function () { return p.shift() }); } // eslint-disable-line
     }
 
     if (opts.compact) { html = html.replace(HTML_PACK, '><$1'); }
@@ -3933,7 +3933,7 @@
     return output
   }
 
-  var version$2 = 'v3.4.0';
+  var version$2 = 'v3.5.1';
 
   var compiler = {
     compile: compile,
--- riot.csp.js
+++ riot.csp.js
@@ -1,4 +1,4 @@
-/* Riot v3.10.1, @license MIT */
+/* Riot v3.10.2, @license MIT */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
@@ -9326,7 +9326,7 @@
     return delete __TAG_IMPL[name]
   }
 
-  var version = 'v3.10.1';
+  var version = 'v3.10.2';
 
   var core = /*#__PURE__*/Object.freeze({
     Tag: Tag,
@@ -9641,7 +9641,7 @@
         // does a tag exist in this position?
         var tag = tags[pos];
         var mustAppend = i >= oldItems.length;
-        var mustCreate = doReorder && isNew || !doReorder && !tag;
+        var mustCreate = doReorder && isNew || !doReorder && !tag || !tags[i];
 
         // new tag
         if (mustCreate) {
