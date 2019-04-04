bdefcb3dfa5fd93bc63d9657c0445e6a325ba102
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-04-04 20:43:28 +0200

    updated: riot bundle

--- riot+compiler.js
+++ riot+compiler.js
@@ -2190,10 +2190,10 @@
     __: __
   });
 
-  var commonjsGlobal = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
+  var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
 
   function unwrapExports (x) {
-  	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x.default : x;
+  	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
   }
 
   function createCommonjsModule(fn, module) {
@@ -2201,7 +2201,7 @@
   }
 
   function getCjsExportFromNamespace (n) {
-  	return n && n.default || n;
+  	return n && n['default'] || n;
   }
 
   var _empty_module = {};
