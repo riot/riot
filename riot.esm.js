56b3a84b717ae6f99a2751b4905d65c9b567020a
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-06-05 22:49:47 +0200

    4.0.8

--- riot.esm.js
+++ riot.esm.js
@@ -1,4 +1,4 @@
-/* Riot v4.0.7, @license MIT */
+/* Riot v4.0.8, @license MIT */
 const COMPONENTS_IMPLEMENTATION_MAP = new Map(),
       DOM_COMPONENT_INSTANCE_PROPERTY = Symbol('riot-component'),
       PLUGINS_SET = new Set(),
@@ -2208,7 +2208,7 @@ function component(implementation) {
 }
 /** @type {string} current riot version */
 
-const version = 'v4.0.7'; // expose some internal stuff that might be used from external tools
+const version = 'v4.0.8'; // expose some internal stuff that might be used from external tools
 
 const __ = {
   cssManager,
