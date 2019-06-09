27f1764e1b3d277bfd057d227c692766adc2f766
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-06-09 23:11:17 +0200

    4.1.0

--- riot.js
+++ riot.js
@@ -1,4 +1,4 @@
-/* Riot v4.0.8, @license MIT */
+/* Riot v4.1.0, @license MIT */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
@@ -2214,7 +2214,7 @@
   }
   /** @type {string} current riot version */
 
-  const version = 'v4.0.8'; // expose some internal stuff that might be used from external tools
+  const version = 'v4.1.0'; // expose some internal stuff that might be used from external tools
 
   const __ = {
     cssManager,
