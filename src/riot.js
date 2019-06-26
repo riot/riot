af9bd231dfa484ded7eb7ee03cc18224a7171954
Hugo Delgado <hfd19345@campus.fct.unl.pt>
2019-06-26 18:19:17 +0100

    feat: Add the keepRootElement argumento to the unmount method

--- src/riot.js
+++ src/riot.js
@@ -54,12 +54,13 @@ export function mount(selector, initialProps, name) {
 /**
  * Sweet unmounting helper function for the DOM node mounted manually by the user
  * @param   {string|HTMLElement} selector - query for the selection or a DOM element
+ * @param   {boolean|null} keepRootElement - if true keep the root element
  * @returns {Array} list of nodes unmounted
  */
-export function unmount(selector) {
+export function unmount(selector, keepRootElement) {
   return $(selector).map(element => {
     if (element[DOM_COMPONENT_INSTANCE_PROPERTY]) {
-      element[DOM_COMPONENT_INSTANCE_PROPERTY].unmount()
+      element[DOM_COMPONENT_INSTANCE_PROPERTY].unmount(keepRootElement)
     }
     return element
   })
