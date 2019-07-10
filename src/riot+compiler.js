b55db8a6abcd61f71e811a0148c31bb616f6a0b7
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-07-10 22:48:20 +0200

    fixed: backslashed css properties

--- src/riot+compiler.js
+++ src/riot+compiler.js
@@ -13,7 +13,7 @@ function evaluate(js, url) {
 
   // make the source available in the "(no domain)" tab
   // of Chrome DevTools, with a .js extension
-  if (url) node.text =  `${js}\n//# sourceURL=${url}.js`
+  if (url) node.text = `${js}\n//# sourceURL=${url}.js`
 
   root.appendChild(node)
   root.removeChild(node)
