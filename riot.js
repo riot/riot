bfa162036d2e4db3a40bf3fce2c3878d9cb92110
Gianluca Guarini <gianluca.guarini@gmail.com>
2020-05-16 10:55:34 +0200

    4.12.3

--- riot.js
+++ riot.js
@@ -1,4 +1,4 @@
-/* Riot v4.12.2, @license MIT */
+/* Riot v4.12.3, @license MIT */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
@@ -2632,7 +2632,7 @@
   }
   /** @type {string} current riot version */
 
-  const version = 'v4.12.2'; // expose some internal stuff that might be used from external tools
+  const version = 'v4.12.3'; // expose some internal stuff that might be used from external tools
 
   const __ = {
     cssManager,
