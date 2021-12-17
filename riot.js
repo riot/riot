31e8916c99f6ef5a46bead8f4080e2408c053200
gianlucaguarini <gianluca.guarini@gmail.com>
2021-12-17 21:09:15 +0100

    6.1.1

--- riot.js
+++ riot.js
@@ -1,4 +1,4 @@
-/* Riot v6.1.0, @license MIT */
+/* Riot v6.1.1, @license MIT */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
@@ -2553,7 +2553,7 @@
   const withTypes = component => component;
   /** @type {string} current riot version */
 
-  const version = 'v6.1.0'; // expose some internal stuff that might be used from external tools
+  const version = 'v6.1.1'; // expose some internal stuff that might be used from external tools
 
   const __ = {
     cssManager,
