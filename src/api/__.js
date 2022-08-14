9a1ba059a3ba7abd3dc47bff00fa85210a26df67
gianlucaguarini <gianluca.guarini@gmail.com>
2022-08-14 20:44:46 +0200

    fixed: eslint warnings

--- src/api/__.js
+++ src/api/__.js
@@ -1,5 +1,5 @@
-import {bindingTypes, createBinding, createExpression,expressionTypes, template} from '@riotjs/dom-bindings'
 import {DOM_COMPONENT_INSTANCE_PROPERTY, PARENT_KEY_SYMBOL} from '@riotjs/util'
+import {bindingTypes, createBinding, createExpression,expressionTypes, template} from '@riotjs/dom-bindings'
 import cssManager from '../core/css-manager'
 
 // expose some internal stuff that might be used from external tools
