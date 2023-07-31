2e0a8fd550bb14bb661acd247442b452946473ec
gianlucaguarini <gianluca.guarini@gmail.com>
2023-07-31 23:34:36 +0200

    fixed: Update saucelabs browsers

--- test/saucelabs-browsers.cjs
+++ test/saucelabs-browsers.cjs
@@ -2,7 +2,7 @@ module.exports = {
   browsers: {
     sl_ios_safari: {
       base: 'SauceLabs',
-      deviceName: 'iPhone',
+      deviceName: 'iPhone Simulator',
       platformVersion: '14.5',
       platformName: 'iOS',
       browserName: 'Safari',
@@ -16,13 +16,11 @@ module.exports = {
     slChrome: {
       base: 'SauceLabs',
       browserName: 'chrome',
-      platform: 'Linux',
     },
-    // Firefox seems to be no longer working on saucelabs https://github.com/mochajs/mocha/pull/4933
-    slFirefox: {
+    // Firefox seems to be no longer working on saucelabs https://github.com/karma-runner/karma-sauce-launcher/issues/275
+    /*slFirefox: {
       base: 'SauceLabs',
       browserName: 'firefox',
-      platform: 'Linux',
-    },
+    },*/
   },
 }
