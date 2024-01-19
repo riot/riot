a107e7b1c3d024e04e292e9558b51c88bbd16962
gianlucaguarini <gianluca.guarini@gmail.com>
2024-01-20 00:23:26 +0100

    fix: Node unit test

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@ import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 const emptyFile = 'export default undefined'
 
-const ignoredModules = ['fs', 'path', 'esprima']
-
 // ignore builtin requires
 function ignore() {
   return {
@@ -25,30 +23,11 @@ export default {
     generatedCode: {
       constBindings: true,
     },
-    globals: ignoredModules.reduce(
-      (acc, dep) => ({
-        [dep]: dep,
-        ...acc,
-      }),
-      {},
-    ),
     entryFileNames: (chunkInfo) =>
       chunkInfo.name.includes('node_modules')
         ? // replace the node_modules from the path in order to avoid nodejs esm conflicts
           `${chunkInfo.name.replace('node_modules', 'dependencies')}.js`
         : '[name].js',
   },
-  external: ignoredModules,
-  plugins: [
-    ignore(),
-    nodeResolve(),
-    commonjs({
-      include: 'node_modules/**',
-      transformMixedEsModules: true,
-      ignoreTryCatch: false,
-      ignoreDynamicRequires: true,
-      exclude: ignoredModules,
-      ignoreGlobal: true,
-    }),
-  ],
+  plugins: [ignore(), nodeResolve(), commonjs()],
 }
