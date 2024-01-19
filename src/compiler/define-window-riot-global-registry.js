7687f2273e3759efd22bcb8ec4fce092ef2b46b8
gianlucaguarini <gianluca.guarini@gmail.com>
2024-01-20 00:23:26 +0100

    fix: Node unit test

--- src/compiler/define-window-riot-global-registry.js
+++ src/compiler/define-window-riot-global-registry.js
@@ -2,6 +2,5 @@ import { GLOBAL_REGISTRY } from './global-registry.js'
 
 /* istanbul ignore next */
 export function defineWindowRiotGlobalRegistry() {
-  if (window[GLOBAL_REGISTRY]) return
-  window[GLOBAL_REGISTRY] = {}
+  window[GLOBAL_REGISTRY] = window[GLOBAL_REGISTRY] || {}
 }
--- src/compiler/evaluate.js
+++ src/compiler/evaluate.js
@@ -1,12 +1,24 @@
-// evaluates a compiled tag within the global context
-export function evaluate(js, url) {
+const isBrowser = typeof process === 'undefined'
+
+/* c8 ignore start */
+const evaluateWithScriptInjection = (code, url) => {
   const node = document.createElement('script')
   const root = document.documentElement
-
   // make the source available in the "(no domain)" tab
   // of Chrome DevTools, with a .js extension
-  if (url) node.text = `${js}\n//# sourceURL=${url}.js`
+  node.text = url ? `${code}\n//# sourceURL=${url}.js` : code
 
   root.appendChild(node)
   root.removeChild(node)
+}
+/* c8 ignore end */
+
+// evaluates a compiled tag within the global context
+export function evaluate(code, url) {
+  // browsers can evaluate the code via script injection and sourcemaps
+  /* c8 ignore start */
+  if (isBrowser) evaluateWithScriptInjection(code, url)
+  /* c8 ignore end */
+  // in other environments we rely on a simple Function eval
+  else new Function(code)()
 }
