5d89946e304fce3bcd1a588fcc54cada1946d765
gianlucaguarini <gianluca.guarini@gmail.com>
2024-01-26 23:02:07 +0100

    fixed: e2e tests

--- test/e2e/test.e2e.js
+++ test/e2e/test.e2e.js
@@ -3,6 +3,12 @@ import { browser, expect } from '@wdio/globals'
 describe('Run the mocha tests', function () {
   it('All the mocha tests passed', async () => {
     await browser.url('http://localhost:3000/test/e2e/')
+    await browser.waitUntil(async () =>
+      browser.execute(() => typeof window.testFailures === 'number'),
+    )
+    const testFailures = await browser.execute(() => window.testFailures)
+
+    expect(testFailures).toBe(0)
     expect(true).toBe(true)
   })
 })
