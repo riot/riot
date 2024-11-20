812c2d2b75e174abbdbaab17eb8a238744a94bb4
Kal-Aster <elitexxl@hotmail.it>
2024-11-20 22:05:11 +0100

    fix formatting

--- test/typing/autobound-methods-type-inference.ts
+++ test/typing/autobound-methods-type-inference.ts
@@ -1,29 +1,32 @@
-import { AutobindObjectMethods, RiotComponent } from "../../riot"
+import { AutobindObjectMethods, RiotComponent } from '../../riot'
 
 type BaseObject = {
-    method(example: number): boolean
+  method(example: number): boolean
 }
-type ThisObject = RiotComponent<{
+type ThisObject = RiotComponent<
+  {
     prop: string
-}, {
+  },
+  {
     stateProp: number
-}>
+  }
+>
 type BoundObject = AutobindObjectMethods<BaseObject, ThisObject>
 
 export const object: BoundObject = {
-    // this method is correctly implemented
-    method(example) {
-        // check also if this is correctly inferred
-        this.onBeforeMount != null
-        return example > 12
-    },
+  // this method is correctly implemented
+  method(example) {
+    // check also if this is correctly inferred
+    this.onBeforeMount != null
+    return example > 12
+  },
 }
 export const object2: BoundObject = {
-    /**
-     * The following implementation will throw
-     * an error because of the wrong arg type
-     */
-    method(example: string) {
-        return example != ""
-    }
+  /**
+   * The following implementation will throw
+   * an error because of the wrong arg type
+   */
+  method(example: string) {
+    return example != ''
+  },
 }
