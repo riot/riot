f683b54eecaae134a135c9bd66e6b22c24c60980
Kal-Aster <elitexxl@hotmail.it>
2024-11-20 22:04:06 +0100

    add test for AutoBindObjectMethods inference

---
+++ test/typing/autobound-methods-type-inference.ts
@@ -0,0 +1,29 @@
+import { AutobindObjectMethods, RiotComponent } from "../../riot"
+
+type BaseObject = {
+    method(example: number): boolean
+}
+type ThisObject = RiotComponent<{
+    prop: string
+}, {
+    stateProp: number
+}>
+type BoundObject = AutobindObjectMethods<BaseObject, ThisObject>
+
+export const object: BoundObject = {
+    // this method is correctly implemented
+    method(example) {
+        // check also if this is correctly inferred
+        this.onBeforeMount != null
+        return example > 12
+    },
+}
+export const object2: BoundObject = {
+    /**
+     * The following implementation will throw
+     * an error because of the wrong arg type
+     */
+    method(example: string) {
+        return example != ""
+    }
+}
