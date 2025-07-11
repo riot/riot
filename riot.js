70053f34a501c851428215b583bf7b37a522cd6b
Gianluca Guarini <gianluca.guarini@gmail.com>
2025-07-11 20:44:03 +0200

    9.4.10

--- riot.js
+++ riot.js
@@ -1,4 +1,4 @@
-/* Riot v9.4.9, @license MIT */
+/* Riot v9.4.10, @license MIT */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
@@ -2536,7 +2536,7 @@
   const withTypes = (component) => component;
 
   /** @type {string} current riot version */
-  const version = 'v9.4.9';
+  const version = 'v9.4.10';
 
   // expose some internal stuff that might be used from external tools
   const __ = {
